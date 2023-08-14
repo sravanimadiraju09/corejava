7358c232b36482e8d0e10d5fa39345f4648d1998
Sravani <sravanilal1986@gmail.com>
2023-08-14 12:59:02 +0530

    1st program committed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to I.T Mastar");
+        System.out.println("Welcome to I.T Mastar!");
     }
 }
